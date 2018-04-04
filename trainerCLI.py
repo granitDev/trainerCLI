8946c078a9111105c4c3968c877d632f9cc34e24
Brandon <granitdev@users.noreply.github.com>
2018-04-03 20:35:54 -0400

    logic update

--- trainerCLI.py
+++ trainerCLI.py
@@ -1,9 +1,35 @@
 import configparser
+import sys
+import getopt
+from random import randint
 
 exConfig = configparser.ConfigParser()
 exConfig.read("exercises.ini")
 
-for sec in exConfig.sections():
-    print(sec)
-    for item in exConfig[sec]:
-        print("    ", item)
+#for arg in sys.argv:
+g_exList = []
+    
+def getExerises():
+    global g_exList
+    for sec in exConfig.sections():
+        if "arm" in sec:
+            tmp = []
+            for item in exConfig[sec]:
+                tmp.append(item)
+            rnd = randint(0, (len(tmp)-1))
+            g_exList.append(tmp[rnd])
+    print(g_exList)
+
+getExerises()
+exit()
+
+def main(argv):
+    try:
+        # g gym, h home, 
+        opts, args = getopt.getopt(argv, "hg:m:", ["help", "gym=", "home="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(-1)
+        
+if __name__ == "__main__":
+    main(sys.argv[1:])
