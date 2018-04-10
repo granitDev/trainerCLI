1e8a1ede4d1e94954b2bd7d0714693af0da6e5d6
Brandon <granitdev@users.noreply.github.com>
2018-04-09 21:44:17 -0400

    updates

--- trainerCLI.py
+++ trainerCLI.py
@@ -12,7 +12,6 @@ g_now = datetime.datetime.now()
 
 #for arg in sys.argv:
 g_exList = []
-
 g_logList = []
 g_lastLog = []
 
@@ -27,8 +26,8 @@ def getLastLog(logList):
     return list(reversed(lastWorkout))
 
 try:
-    with open("training.log","r") as L:
-        for line in L:
+    with open("training.log","r") as log:
+        for line in log:
             line = line[:-1]
             g_logList.append(line)
         g_lastLog = getLastLog(g_logList)
@@ -38,6 +37,32 @@ except (OSError, IOError) as e:
     print(e)
     print("Log file not found")
     open("training.log", "w+").close()
+    
+class ExerData:
+    'Holds all the data for a specific exercise'
+    def __init__(self, sec, name, cats, data):
+        self.sec = sec
+        self.name = name
+        self.cats = cats
+        self.data = data
+        self.low = []
+        self.med = []
+        self.hi = []
+        self.hit = []
+    
+for sec in exConfig.sections():
+    if sec != "rest":
+        for ex in exConfig[sec]:
+            data = exConfig[sec][ex]
+            data = data.split("-")
+            cats = data[0]
+            exda = ExerData(sec, ex, cats, data)
+            g_exList.append(exda)
+        
+for x in g_exList:
+    print(x.name, "  ", x.sec)
+        
+exit()
 
 def getRandEx(exList):
     return exList[randint(0, (len(exList)-1))]
@@ -70,18 +95,18 @@ def getArms(location):
     
     return exList
     
-def buildWorkout(site):
+def buildWorkout(location):
     global exConfig, g_now, g_lastLog
     exList = []
     
-    for ex in getArms(site):
+    for ex in getArms(location):
         exList.append(ex)
         
     for sec in exConfig.sections():
         if "hand" in sec:
-            exList.append(getNext(sec, g_lastLog[3], site))
+            exList.append(getNext(sec, g_lastLog[3], location))
         if "leg" in sec:
-            exList.append(getNext(sec, g_lastLog[4], site))
+            exList.append(getNext(sec, g_lastLog[4], location))
             #g_exList.append(exConfig[sec].get(exercise))
     with open("training.log","a+") as f:
         f.write("@ {}\n".format(g_now.strftime("%Y-%m-%d %A")))
